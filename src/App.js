dd712ca1ec255ae236ee000c6cea19d70abf6348
shahnmah <shahn.mah@gmail.com>
2022-06-30 23:33:50 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div>
+    <div style={{backgroundColor: '#EFFFFD'}}>
       <Header />
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            
+            <Navbar bg="transparent" variant="light">
+                <Container className='menubar'>
+                    <Navbar.Brand href="#home">
+                        <Link to='/home'><img className='w-75' src="https://i.ibb.co/VC2HChc/taskzo-logo.png" alt="logo" /></Link>
+                    </Navbar.Brand>
+                    <Nav className=" menu">
+                        <Link to='/home'>Completed Tasks</Link>
+                        <Link to='/home'>To-Do</Link>
+                        <Link to='/home'>Calendar</Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </div>
     );
 };
